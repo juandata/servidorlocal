0775ad6f29f73c10578e80e1c1ab7e1da0ab899c
Juan David Tabares Arce <juandavidtabaresarce@gmail.com>
2018-07-23 16:46:07 -0400

    updating

--- server/utils/textSchema.js
+++
@@ -1,7 +0,0 @@
-let mongoose = require('mongoose');
-let textSchema = mongoose.Schema({
-    filename  : String,
-    contentType : String
-
-});
-module.exports = mongoose.model("Texto", textSchema);
