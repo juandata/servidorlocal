3198168296f25e6dec0852d874be3e62727dec97
Juan David Tabares Arce <juandavidtabaresarce@gmail.com>
2018-07-23 17:15:40 -0400

    added heroku url to proxy

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     port: 3000,
     open: true,
     proxy: {
-      '/' : 'http://localhost:8080'
+      '/' : 'https://servidorlocal.herokuapp.com:8080'
     }
   },
   plugins: [
